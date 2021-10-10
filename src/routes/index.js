0a84363c2158c9c06807abc6f547b233e6a26254
Yan <15334762+yanlow@users.noreply.github.com>
2021-10-10 21:59:31 +0800

    Removed VueJS; added raw JS interval querying

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,41 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
-const Vue = require('vue')
 
 const template = require('fs').readFileSync('./views/index.template.html', 'utf-8');
-const renderer = require('vue-server-renderer').createRenderer({
-  template,
-});
-
-const context = {
-  title: 'vue ssr',
-  metas: `
-      <meta name="keyword" content="vue,ssr">
-      <meta name="description" content="vue srr demo">
-  `,
-};
-
-var data = _.fill(Array(20160), {});
 
 router.get('/', function(req, res, next) {
-  const app = new Vue({
-    data: {
-      url: req.url
-    },
-    template: `<div>The visited URL is: {{ url }}</div>`,
-  });
-
-  renderer
-  .renderToString(app, context, (err, html) => {
-    console.log(html);
-    if (err) {
-      res.status(500).end('Internal Server Error')
-      return;
-    }
-    res.end(html);
-  });
-
+  res.end(template);
 });
 
 module.exports = router;
