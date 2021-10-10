947c156e57c6d132c40159f83c3734451009622a
Yan <15334762+yanlow@users.noreply.github.com>
2021-10-10 21:38:44 +0800

    VueJS SSR

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+const Vue = require('vue')
+
+const template = require('fs').readFileSync('./views/index.template.html', 'utf-8');
+const renderer = require('vue-server-renderer').createRenderer({
+  template,
+});
+
+const context = {
+  title: 'vue ssr',
+  metas: `
+      <meta name="keyword" content="vue,ssr">
+      <meta name="description" content="vue srr demo">
+  `,
+};
 
 var data = _.fill(Array(20160), {});
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("<html><body><h1>Banana Weather Station</b><br />" + JSON.stringify(data) + "</body></html>");
-});
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`,
+  });
 
-/* POST home page. */
-router.post('/', function(req, res, next) {
-  data.unshift(req.body)
-  data.pop()
-  res.sendStatus(200);
+  renderer
+  .renderToString(app, context, (err, html) => {
+    console.log(html);
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return;
+    }
+    res.end(html);
+  });
+
 });
 
 module.exports = router;
